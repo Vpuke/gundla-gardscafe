147d1ebc50a289e1ec24bc55ff4099e6b38a0035
Michaela Lundborg <54806234+lundborgm@users.noreply.github.com>
2020-09-01 15:47:16 +0200

    Update index.js

--- web/pages/index.js
+++ web/pages/index.js
@@ -1,9 +1,19 @@
-const Index = () => {
+import client from '../client'
+
+const Index = (props) => {
+  console.log(props);
   return (
     <div>
-      <p>Hello world!</p>
+      <h1>{props.name}</h1>
+      <p>{props.description}</p>
     </div>
   );
 };
-
+Index.getInitialProps = async function(context) {
+  // It's important to default the slug so that it doesn't return "undefined"
+  const { slug = "" } = context.query
+  return await client.fetch(`
+    *[_type == "eventInformation"][0]
+  `, { slug })
+}
 export default Index;
