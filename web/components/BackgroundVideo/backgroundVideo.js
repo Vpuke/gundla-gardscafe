35dd410ea3512172c472ca0c04133384d52ac1b0
Viktor <viktorpuke@gmail.com>
2020-09-18 15:45:05 +0200

    responsive video background

--- web/components/BackgroundVideo/backgroundVideo.js
+++ web/components/BackgroundVideo/backgroundVideo.js
@@ -1,38 +1,46 @@
 import styled from "styled-components";
 
 const StyledBackgroundVideo = styled.div`
-  .backgroundVideo {
-    overflow: hidden;
+  overflow: hidden;
+  background-size: cover;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+
+  .videoSource {
     position: absolute;
-    height: 100vh;
-    width: 100%;
     left: 0;
     top: 0;
-    right: 0;
     z-index: 1;
-    background-size: cover;
+    width: 100%;
   }
 
-  videoSource {
-    width: 100%;
-    height: 100%;
-    object-position: center bottom;
-    object-fit: fill;
+  @media (min-aspect-ratio: 16/9) {
+    .videoSource {
+      width: 100%;
+      height: auto;
+    }
+  }
+  @media (max-aspect-ratio: 16/9) {
+    .videoSource {
+      width: auto;
+      height: 100%;
+    }
   }
 `;
 
 const BackgroundVideo = () => {
-  const videoSourceSmall = "/hero-video.mp4";
+  // const videoSourceSmall = "/hero-video.mp4";
   const videoSourceLarge = "/hero-desktop.mp4";
 
   return (
     <StyledBackgroundVideo>
-      <div className="backgroundVideo">
-        <video className="videoSource" autoPlay="autoplay" loop="loop" muted>
-          <source src={videoSourceLarge} type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
-      </div>
+      {/* <div className="backgroundVideo"> */}
+      <video className="videoSource" autoPlay="autoplay" loop="loop" muted>
+        <source src={videoSourceLarge} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+      {/* </div> */}
     </StyledBackgroundVideo>
   );
 };
