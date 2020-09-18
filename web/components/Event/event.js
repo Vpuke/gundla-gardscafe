678e7e631ca5a091df6f4023782864023195ddd9
Michaela Lundborg <54806234+lundborgm@users.noreply.github.com>
2020-09-18 16:41:35 +0200

    event page updates

--- web/components/Event/event.js
+++ web/components/Event/event.js
@@ -29,28 +29,32 @@ a {
   border-left: 2px solid #fff;
   padding-left: 10px;
   cursor: pointer;
+  font-family: IBM Plex Sans;
+  font-weight: 400;
+  font-size: 16px;
+  text-align: left;
+  cursor: pointer;
+  text-decoration: none;
 }
 
 .images {
-  background: grey;
-  height: 186px;
   widht: 100%;
-  margin: 36px -15px;
+  margin: 20px 0;
+  overflow: hidden;
   display: flex;
-  flex-direction: row;
+  align-items: center;
+  justify-content: center;
 }
 
 .image-container {
-  widht: 50%;
-  overflow: hidden;
-  object-fit: cover;
+ 
+  width: 268px;
 }
 
 img {
+  height: auto;
   width: 100%;
-  height: 100%;
   object-fit: cover;
-  display: block;
 }
 
 .forms {
@@ -60,13 +64,28 @@ img {
   margin: 24px 0 20px 0;
 }
 
+.form-btn {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+small {
+  font-family: "IBM Plex Sans", sans-serif;
+  font-weight: 300;
+  font-size: 12px;
+}
+
 .contact p {
   margin: 0;
 }
 
+.contact .bold {
+  font-weight: 500;
+}
+
 .contact a {
-  background: white;
-  color: black;
+  font-size: 18px;
 }
 `;
 
@@ -92,22 +111,25 @@ const Event = ({ data }) => {
         </Link>
         <div className="images">
           <div className="image-container">
-            <img src="/location.png"></img>
-          </div>
-          <div className="image-container">
-            <img src="/location.png"></img>
+            <img src="/yoga-mobile.png"></img>
           </div>
         </div>
         <div className="contact">
-          <p>Vill du boka ett eget event eller catering?</p>
-          <p>Tryck här nedan för att komma till våra formulär där ni får fylla i mer info.</p>
+          <p className="bold">Vill du boka ett eget event eller catering?</p>
+          <p>Tryck här nedan för att komma till våra formulär.</p>
           <div className="forms">
-            <Link href="/contact">
-              <a>Fomulär</a>
-            </Link>
-            <Link href="/contact">
-              <a>Fomulär</a>
-            </Link>
+            <div className="form-btn">
+              <Link href="/contact">
+                <a>Boka event</a>
+              </Link>
+              <small>Ha ditt evenemang hos oss</small>
+            </div>
+            <div className="form-btn">
+              <Link href="/contact">
+                <a>Boka catering</a>
+              </Link>
+              <small>Mat till ditt privata evenemang</small>
+            </div>
           </div>
         </div>
       </div>
